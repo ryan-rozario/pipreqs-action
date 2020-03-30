e04485748cf0613343b66939f2043ae219abe56a
ryan-rozario <ryan.rozario1999@gmail.com>
2020-03-30 23:19:09 +0530

    fixed empty commit message

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ set -e
 
 sh -c "git config --global user.name '${GITHUB_ACTOR}' \
       && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
-      && git add -A && git commit -m '$*' --allow-empty \
+      && git add -A && git commit -m 'updated requirement file' \
       && git push -u origin HEAD"
