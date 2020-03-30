24b59a7eaf4dd9408459867c800dacea7ac6f0f7
ryan-rozario <ryan.rozario1999@gmail.com>
2020-03-30 23:45:04 +0530

    commit requirement file to repo

--- src/create-requirement.py
+++ src/create-requirement.py
@@ -4,7 +4,7 @@ from pipreqs import pipreqs
 
 def main():
     p=os.environ.get("PROJECT_PATH")
-    out = "/github/workspace/" + str(os.environ.get("REQUIREMENT_PATH"))
+    out = str(os.environ.get("REQUIREMENT_PATH"))
     pipreqs.init({'<path>':p, '--savepath': out, '--print': True,'--use-local': None, '--force': True, '--proxy':None, '--pypi-server':None,'--diff': None, '--clean': None})
 
 if __name__ == "__main__":
