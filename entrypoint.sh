5e6b773f67482b4c145b4372098c0de74f02a539
ryan-rozario <ryan.rozario1999@gmail.com>
2020-03-31 00:10:42 +0530

    commit requirement file to repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 
 echo "$REQUIREMENT_PATH"
-python3 /create-requirement.py > "$REQUIREMENT_PATH"
+#python3 /create-requirement.py > "$REQUIREMENT_PATH"
 
-set -e
-sh -c "ls"
+#set -e
+#sh -c "ls"
 
-sh -c "git config --global user.name '${GITHUB_ACTOR}' \
-      && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
-      && git add -A && git commit -m 'updated requirement file' \
-      && git push -u origin HEAD"
+#sh -c "git config --global user.name '${GITHUB_ACTOR}' \
+#      && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
+#      && git add -A && git commit -m 'updated requirement file' \
+#      && git push -u origin HEAD"
