32b5db31ba51721d200438232dbf1cf97bca3a8c
ryan-rozario <ryan.rozario1999@gmail.com>
2020-03-31 01:03:01 +0530

    tests

--- src/create-requirement.py
+++ src/create-requirement.py
@@ -4,9 +4,9 @@ from pipreqs import pipreqs
 
 def main():
     p = os.environ.get("INPUT_PROJECT_PATH")
-    pipreqs.init({'<path>': p, '--savepath': None, '--print': True, 
-                    '--use-local': None, '--force': True, '--proxy': None, 
-                    '--pypi-server': None, '--diff': None, '--clean': None})
+    pipreqs.init({'<path>': p, '--savepath': None, '--print': True,
+                  '--use-local': None, '--force': True, '--proxy': None,
+                  '--pypi-server': None, '--diff': None, '--clean': None})
 
 
 if __name__ == "__main__":
