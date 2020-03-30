38841e1a662973ba56e31b0ae5a5634be2bff533
ryan-rozario <ryan.rozario1999@gmail.com>
2020-03-30 23:14:16 +0530

    commit requirement file to repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -l
 
 python3 /create-requirement.py
+
+set -e
+
+sh -c "git config --global user.name '${GITHUB_ACTOR}' \
+      && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
+      && git add -A && git commit -m '$*' --allow-empty \
+      && git push -u origin HEAD"
--- src/create-requirement.py
+++ src/create-requirement.py
@@ -6,7 +6,6 @@ def main():
     p=os.environ.get("PROJECT_PATH")
     out = "/github/workspace/" + str(os.environ.get("REQUIREMENT_PATH"))
     pipreqs.init({'<path>':p, '--savepath': out, '--print': True,'--use-local': None, '--force': True, '--proxy':None, '--pypi-server':None,'--diff': None, '--clean': None})
-    print(os.environ.get("GITHUB_ACTOR"))
 
 if __name__ == "__main__":
     main()
