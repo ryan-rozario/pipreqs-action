bf19178086f58c1865f6d13dbb9e99d9a3d576b0
ryan-rozario <ryan.rozario1999@gmail.com>
2020-03-31 00:14:02 +0530

    commit requirement file to repo

--- src/create-requirement.py
+++ src/create-requirement.py
@@ -5,7 +5,8 @@ from pipreqs import pipreqs
 def main():
     p=os.environ.get("PROJECT_PATH")
     pipreqs.init({'<path>':p, '--savepath': None, '--print': True,'--use-local': None, '--force': True, '--proxy':None, '--pypi-server':None,'--diff': None, '--clean': None})
-
+    print("req   :"+os.environ.get("REQUIREMENT_PATH"))
+    print(p)
 
 if __name__ == "__main__":
     main()
