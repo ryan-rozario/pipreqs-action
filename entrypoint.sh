aca5914ac3ab2b5060d10d712eb9f55ba035b20e
Afonso Vale <38760902+afonsoVale@users.noreply.github.com>
2020-09-12 01:11:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ sh -c "ls"
 
 sh -c "git config --global user.name '${GITHUB_ACTOR}' \
       && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
-      && git add -A && ((git commit -m 'updated requirement file' && git push -u origin HEAD) || echo "No changes to requirements file")"
+      && git add -A && [[git commit -m 'updated requirement file' && git push -u origin HEAD] || echo "No changes to requirements file"]"
