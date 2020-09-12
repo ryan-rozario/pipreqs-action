3de4e5e523a732a91b4ae8259f96103b008c2fe9
Afonso Vale <38760902+afonsoVale@users.noreply.github.com>
2020-09-12 01:38:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ set -e
 sh -c "ls"
 
 sh -c "git config --global user.name '${GITHUB_ACTOR}' \
-      && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
-      && echo `git add -A && git commit -m "updated requirement file" && git push -u origin HEAD`"
+      && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com'"
+echo `git add -A && git commit -m "updated requirement file" && git push -u origin HEAD`
