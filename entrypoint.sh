d89b6fb658b97188a47cde6907520d21234aaa15
Afonso Vale <38760902+afonsoVale@users.noreply.github.com>
2020-09-12 01:05:01 +0100

    Fixed an error when there are no changes to the file

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ sh -c "ls"
 
 sh -c "git config --global user.name '${GITHUB_ACTOR}' \
       && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
-      && git add -A && git commit -m 'updated requirement file' \
+      && git add -A && (git commit -m 'updated requirement file' || echo "No changes to requirements file") \
       && git push -u origin HEAD"
