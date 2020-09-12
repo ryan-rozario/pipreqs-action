33b7636f1b20fa7131a64b38aa2e6acac352f25f
Afonso Vale <38760902+afonsoVale@users.noreply.github.com>
2020-09-12 01:45:00 +0100

    Added support for custom commit titles

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ sh -c "ls"
 
 sh -c "git config --global user.name '${GITHUB_ACTOR}' \
       && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com'"
-echo `git add -A && git commit -m "updated requirement file" && git push -u origin HEAD`
+echo `git add -A && git commit -m "Updated $PROJECT_NAME requirements file" && git push -u origin HEAD`
