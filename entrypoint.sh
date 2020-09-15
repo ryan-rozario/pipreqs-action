4520fcebf3a9df5c6bd49ea2d988ff521b26d04b
Afonso Vale <38760902+afonsoVale@users.noreply.github.com>
2020-09-15 19:15:31 +0100

    Fixes #1 (#2)
    Thank you so much for the fix
    
    * Update entrypoint.sh
    
    * Fixed an error when there are no changes to the file
    
    * Added support for custom commit titles

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh -l
 
-python3 /create-requirement.py > "$INPUT_REQUIREMENT_PATH"
+pipreqs --force --savepath $INPUT_REQUIREMENT_PATH $INPUT_PROJECT_PATH
 
 set -e
 sh -c "ls"
 
 sh -c "git config --global user.name '${GITHUB_ACTOR}' \
-      && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
-      && git add -A && git commit -m 'updated requirement file' \
-      && git push -u origin HEAD"
+      && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com'"
+echo `git add -A && git commit -m "Updated $INPUT_PROJECT_NAME requirements file" && git push -u origin HEAD`
